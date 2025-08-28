1d60d300de4da3a67c708a863d6bdfcc7de0ba02
alxpg <alxpega@gmail.com>
2025-08-27 23:59:52 -0600

    Proyecto de React GuitarLA

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+//Como crear componentes en React
+//Los componentes siempre inician con mayúscula y deben ser funciones o clases con su retorno (return)
+//Se deben de importar en el archivo donde se van a utilizar al principio del archivo
+
+//import App from "./src"
+
+function Header () {
+
+    return (
+    <>
+    <h2>Usando componentes desde otro archivo</h2>
+    </>
+    
+    )
+}
+export default Header
---
+++ src/components/Structure.jsx
@@ -0,0 +1,12 @@
+//Estructura de un componente en React
+//1. Importar React
+//2. Crear la función del componente
+//3. Retornar el JSX que se quiere renderizar
+
+function Componente() { //funcion 
+    return ( //retorno al HTML
+        <h1>Hola Mundo</h1> //elementos de html
+    )
+}
+
+export default Componente //exportar el componente para usarlo en otros archivos
